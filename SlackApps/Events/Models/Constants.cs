80ddced6257bfb1327b1a597496922dec78cd33f
Carson Cook <CarsonCook@users.noreply.github.com>
2017-08-29 12:18:53 -0400

    removed tokens

--- SlackApps/Events/Models/Constants.cs
+++ SlackApps/Events/Models/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public const string VERIFICATION_TOKEN = "YrllvtnMXKLoBthNiLO71kxl";
-        public const string BOT_TOKEN = "xoxp-142269157382-142080484080-233242429859-37fd635f6066ecbb0e604c410527538a";
+        public const string VERIFICATION_TOKEN = "";
+        public const string BOT_TOKEN = "";
     }
 }
